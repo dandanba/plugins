eefdea937d860b404cfdb030f276259172276a0d
geng <13641194007@139.com>
2016-08-03 18:14:24 +0800

    bat

---
+++ git_clone.bat
@@ -0,0 +1,24 @@
+mkdir plugins
+cd plugins
+mkdir maitian
+git clone https://github.com/dandanba/appcompat.git
+git clone https://github.com/dandanba/base.git
+git clone https://github.com/dandanba/eventbus.git
+git clone https://github.com/dandanba/glide.git
+git clone https://github.com/dandanba/jackson.git
+git clone https://github.com/dandanba/leakcanary.git
+git clone https://github.com/dandanba/logger.git
+git clone https://github.com/dandanba/model.git
+git clone https://github.com/dandanba/okhttp.git
+git clone https://github.com/dandanba/rx.git
+git clone https://github.com/dandanba/rxbinding.git
+git clone https://github.com/dandanba/transform.git
+git clone https://github.com/dandanba/util.git
+git clone https://github.com/dandanba/retrofit.git
+git clone https://github.com/dandanba/manifest.git
+cd maitian
+git clone https://github.com/dandanba/component.git
+git clone https://github.com/dandanba/api.git
+git clone https://github.com/dandanba/retrofit_api.git
+git clone https://github.com/dandanba/res.git
+git clone https://github.com/dandanba/launcher.git
