f8bc21fe4225a0b12904ba674e7d9572003971c5
geng <13641194007@139.com>
2016-08-25 15:04:59 +0800

    update

---
+++ maitian.bat
@@ -0,0 +1,8 @@
+rmdir /s maitian
+mkdir maitian
+cd maitian
+git clone https://github.com/dandanba/common.git
+start cmd /k "cd common&&uploadArchive&&exit"
+git clone https://github.com/dandanba/maitian.git
+start cmd /k "cd maitian&&uploadArchive&&exit"
+git clone https://github.com/dandanba/maitianapp.git
